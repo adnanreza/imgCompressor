89ebd778cff0f1702927d8cfb5828ae2bb297f03
Adnan Reza <adnan.reza@outlook.com>
2020-05-22 09:01:37 -0700

    Refactored menu to use builtin roles

--- main.js
+++ main.js
@@ -40,15 +40,21 @@ app.on('ready', () => {
 const menu = [
   ...(isMac ? [{ role: 'appMenu' }] : []),
   {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Quit',
-        accelerator: isMac ? 'Command+W' : 'Ctrl+W', //CmdOrCtrl+W
-        click: () => app.quit(),
-      },
-    ],
+    role: 'fileMenu',
   },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
 ];
 
 // Quit when all windows are closed.
