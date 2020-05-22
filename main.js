5917985ec9c9b9dc59030417b4191f452b861d0e
Adnan Reza <adnan.reza@outlook.com>
2020-05-21 20:27:57 -0700

    Set platform specific config and Added boilerplate code to exit app on mac

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 
+// Set environment
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isWin = process.platform === 'win32' ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -8,9 +14,27 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: './assets/icons/Icon_256x256.png',
+    resizable: isDev ? true : false,
   });
 
   mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
